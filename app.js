81cbf113604e24c2728a8f4c0ce8c9309cf63e6c
Luke Eastman <LukeAEastman@gmail.com>
2020-08-31 13:23:23 -0700

    complete react mini sprint

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
 // TODO
+var GroceryList = (props) => (
+  <div>
+    <ul>
+      {props.items.map(item => <GroceryListItem item={item} />)}
+    </ul>
+  </div>
+);
+class GroceryListItem extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      done:false
+    }
+  };
+
+
+  toggleGroceryItemHover () {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  render() {
+    var style = {
+      fontWeight: this.state.done ? 'bold' : 'normal'
+    };
+
+    return (
+      <li style={style} onMouseEnter={this.toggleGroceryItemHover.bind(this)} onMouseLeave={this.toggleGroceryItemHover.bind(this)}>{this.props.item}</li>
+    );
+  }
+};
+
+
+ReactDOM.render(<GroceryList items={['Bread', 'Cheese', 'Lettuce', 'Meat']}/>, document.getElementById("app"));
